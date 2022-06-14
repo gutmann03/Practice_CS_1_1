25ccceec52eab74590af68e72f6f69a96527a4e5
volodymyr smetaniuk <volodymyr.smetaniuk@gmail.com>
2022-06-14 15:34:59 +0300

    grapher oop done

--- graphBuilder.py
+++ graphBuilder.py
@@ -1,33 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import mathGraphBuilder as mgb
+from tkinter import filedialog as fd
 
 # startPoint = -1
 # endPoint = 0.7
 # step = 0.006
-def buildGrapg(startPoint:float, endPoint:float, step:float):
-        xS, yUppers, yLowers = mgb.getCoordinates(startPoint, endPoint, step)
+class GraphBuilder:
+        def __init__(self) -> None:
+                self.fig = plt.figure()
+        
+        def buildGraph(self, startPoint:float, endPoint:float, step:float):
+                xS, yUppers, yLowers = mgb.getCoordinates(startPoint, endPoint, step)
+                ax = self.fig.add_subplop()
+                ax.arrow(-2.0, 0, 4, 0, color = 'black', width = 0.01)
+                ax.arrow(0, -2.5, 0, 5, color = 'black', width = 0.01)
+                ax.plot(xS, yUppers, color='red')
+                ax.plot(xS, yLowers, color='red')
 
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
+                ax.text(1.8, -0.2, 'x', fontsize = 10)
+                ax.text(0.01, 2.0, 'y', fontsize =  10)
 
-        ax.arrow(-2.0, 0, 4, 0,
-                color = 'black',
-                width = 0.01)
+                self.ax = ax
 
-        ax.arrow(0, -2.5, 0, 5,
-                color = 'black',
-                width = 0.01)
+        def saveWithPath(self):
+                path = fd.askdirectory(title="save graph")
+                self.fig.savefig(f"{path}", dpi=300)
+                self.fig.close()
 
-        ax.plot(xS, yUppers, color='red')
-        ax.plot(xS, yLowers, color='red')
-
-        ax.text(1.8, -0.2, 'x',
-                fontsize = 10)
-
-        ax.text(0.01, 2.0, 'y',
-                fontsize = 10)
-
-        plt.savefig('tempPic.png', dpi=300)
-        # plt.show()
-        #tempPic.png || C:\Users\Volodymyr\OneDrive - National University of Kyiv Mohyla Аcademy\пактика_1_курс\Graph_builder\tempPic.png
+        def save(self):
+                self.fig.savefig("tempPic.png", dpi=300)
+                self.fig.close()
--- gui.py
+++ gui.py
@@ -4,19 +4,7 @@ from tkinter import filedialog as fd
 import graphBuilder as gb
 import mathGraphBuilder as mgb
 
-# def saveGrapg(img):
-#     def innerFunc():
-#         #contents = PhotoImage(file=img)
-#         new_file = fd.askdirectory(title='save graph')
-#         print(new_file)
-#         # save(str(new_file) + '.png', 'PNG')
-#         # if new_file:
-#         #     new_file.write(contents)
-#         #     new_file.close()
-#     return innerFunc
-
-    
-
+grapher = gb.GraphBuilder()
 root = Tk()
 root.title('Graph builder')
 root.geometry('810x485+100+100')
@@ -64,15 +52,19 @@ def buildGrapg():
     lblScreen.config(text=outputString)
     
     if len(outputString) == 0:
-        gb.buildGrapg(startPos, endPos, step)
+        grapher.buildGraph(startPos, endPos, step)
+        grapher.save()
         newGraph = PhotoImage(file='tempPic.png').subsample(3, 3)
         lblGrapg.config(image=newGraph)
         # lblScreen.config(text='')
 
+def saveGraph():
+    grapher.saveWithPath()
+
 btnBuildGrapg = Button(text='build', font=24, command=buildGrapg)
 btnBuildGrapg.place(x=5, y=90)
 
-btnSaveGraph = Button(text='save', font=24)#, command=saveGrapg('tempPic.png'))
+btnSaveGraph = Button(text='save', font=24, command=saveGraph)
 btnSaveGraph.place(x=62, y=90)
 
 btnInfo = Button(text='info', font=24)
